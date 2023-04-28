43829c493558fa97e77dddf54196d7d5d59594ec
Carlo Sonday <angelosonday@gmail.com>
2023-04-28 15:44:04 +0800

    complete module3a

--- IE271caseapp/apps/movies/movies_home.py
+++ IE271caseapp/apps/movies/movies_home.py
@@ -73,5 +73,48 @@ layout = html.Div(
 )
 
 
+@app.callback(
+    [
+        Output('moviehome_movielist', 'children')
+    ],
+    [
+        Input('url', 'pathname'),
+        Input('moviehome_titlefilter', 'value'), # changing the text box value should update the table
+    ]
+)
+def moviehome_loadmovielist(pathname, searchterm):
+    print(pathname)
+    if pathname == '/movies':
+        # 1. Obtain records from the DB via SQL
+        # 2. Create the html element to return to the Div
+        sql = """ SELECT movie_name, genre_name
+            FROM movies m
+                INNER JOIN genres g ON m.genre_id = g.genre_id
+            WHERE 
+                NOT movie_delete_ind
+        """
+        values = [] # blank since I do not have placeholders in my SQL
+        cols = ['Movie Title', 'Genre']
+        
+        
+        ### ADD THIS IF BLOCK
+        if searchterm:
+            # We use the operator ILIKE for pattern-matching
+            sql += " AND movie_name ILIKE %s"
+            
+            # The % before and after the term means that
+            # there can be text before and after
+            # the search term
+            values += [f"%{searchterm}%"]
 
-
+        df = db.querydatafromdatabase(sql, values, cols)
+        
+        if df.shape: # check if query returned anything
+            table = dbc.Table.from_dataframe(df, striped=True, bordered=True,
+                    hover=True, size='sm')
+            return [table]
+        else:
+            return ["No records to display"]
+        
+    else:
+        raise PreventUpdate
